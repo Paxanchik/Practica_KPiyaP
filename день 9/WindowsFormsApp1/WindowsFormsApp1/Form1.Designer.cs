84b80dba8eaec4aad1ae3be15196bbfa5c83595b
PaXan <116594902+Paxanchik@users.noreply.github.com>
2023-04-28 09:28:37 +0300

    Add files via upload

---
+++ день 9/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(21, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(309, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(21, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(309, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(21, 90);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(309, 52);
+            this.textBox4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 178);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "день 19";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox textBox3;
+        public System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ день 9/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Создаем новый экземпляр Word
+            Word word = new Word(2019);
+
+            // Создаем новый вирус и запускаем его
+            Virus virus = new Virus();
+            virus.Infect(textBox1);
+
+            // Создаем новый экземпляр CConficker и запускаем его
+            CConficker cconficker = new CConficker();
+            cconficker.Infect(textBox2);
+
+            // Создаем новый экземпляр Minesweeper и запускаем игру
+            Minesweeper minesweeper = new Minesweeper();
+            minesweeper.Play(textBox3);
+
+            // Создаем нового разработчика и пусть он разработает новое ПО
+            Developer developer = new Developer("Иван Иванов");
+            Software software = new Software("Новая программа", 1);
+            developer.DevelopSoftware(software, textBox4);
+        }
+    }
+
+    public abstract class OperationSet
+    {
+        public abstract void AddOperation(string operationName);
+        public abstract void RemoveOperation(string operationName);
+    }
+    public class TextProcessor
+    {
+        private OperationSet _operationSet;
+
+        public TextProcessor(OperationSet operationSet)
+        {
+            _operationSet = operationSet;
+        }
+
+        public void AddOperation(string operationName)
+        {
+            _operationSet.AddOperation(operationName);
+        }
+
+        public void RemoveOperation(string operationName)
+        {
+            _operationSet.RemoveOperation(operationName);
+        }
+    }
+    public class Software
+    {
+        private string _name;
+        private int _version;
+
+        public Software(string name, int version)
+        {
+            _name = name;
+            _version = version;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int Version
+        {
+            get { return _version; }
+        }
+    }
+    public class Word : Software
+    {
+        public Word(int version) : base("Microsoft Word", version)
+        {
+        }
+    }
+    public class Virus
+    {
+        public virtual void Infect(TextBox textBox)
+        {
+            textBox.Text = ("Компьютер заражен вирусом.");
+        }
+    }
+    public class CConficker : Virus
+    {
+        public override void Infect(TextBox textBox)
+        {
+           textBox.Text = ("Компьютер заражен вирусом Conficker.");
+        }
+    }
+
+    // Класс, представляющий игрушку
+    public class Toy
+    {
+        public virtual void Play(TextBox textbox)
+        {
+            textbox.Text = ("Игрушка играет.");
+        }
+    }
+
+    // Класс, представляющий игру Сапер
+    public class Minesweeper : Toy
+    {
+        public override void Play(TextBox textbox)
+        {
+            textbox.Text = ("Игра Сапер запущена.");
+        }
+    }
+    public class Developer
+    {
+        private string _name;
+
+        public Developer(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public void DevelopSoftware(Software software, TextBox textbox)
+        {
+            textbox.Text = ($"{_name} разработал новое программное обеспечение: {software.Name} версии {software.Version}.");
+        }
+    }
+}
