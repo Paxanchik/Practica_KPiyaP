785af49590e4dd477d1a47f7771225cbf11e371e
PaXan <116594902+Paxanchik@users.noreply.github.com>
2023-05-10 10:27:40 +0300

    Add files via upload

---
+++ день 16/App/App/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.arraySizeLabel = new System.Windows.Forms.Label();
+            this.arrayContentsLabel = new System.Windows.Forms.Label();
+            this.elementTextBox = new System.Windows.Forms.TextBox();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.containsButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.multiplierTextBox = new System.Windows.Forms.TextBox();
+            this.characterTextBox = new System.Windows.Forms.TextBox();
+            this.removeNegativeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(19, 233);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // arraySizeLabel
+            // 
+            this.arraySizeLabel.AutoSize = true;
+            this.arraySizeLabel.Location = new System.Drawing.Point(12, 19);
+            this.arraySizeLabel.Name = "arraySizeLabel";
+            this.arraySizeLabel.Size = new System.Drawing.Size(62, 13);
+            this.arraySizeLabel.TabIndex = 1;
+            this.arraySizeLabel.Text = "Элементы:";
+            // 
+            // arrayContentsLabel
+            // 
+            this.arrayContentsLabel.AutoSize = true;
+            this.arrayContentsLabel.Location = new System.Drawing.Point(12, 57);
+            this.arrayContentsLabel.Name = "arrayContentsLabel";
+            this.arrayContentsLabel.Size = new System.Drawing.Size(49, 13);
+            this.arrayContentsLabel.TabIndex = 2;
+            this.arrayContentsLabel.Text = "Массив:";
+            // 
+            // elementTextBox
+            // 
+            this.elementTextBox.Location = new System.Drawing.Point(19, 134);
+            this.elementTextBox.Name = "elementTextBox";
+            this.elementTextBox.Size = new System.Drawing.Size(100, 20);
+            this.elementTextBox.TabIndex = 3;
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Location = new System.Drawing.Point(111, 233);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(75, 23);
+            this.multiplyButton.TabIndex = 4;
+            this.multiplyButton.Text = "Умножить";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.multiplyButton_Click);
+            // 
+            // containsButton
+            // 
+            this.containsButton.Location = new System.Drawing.Point(204, 233);
+            this.containsButton.Name = "containsButton";
+            this.containsButton.Size = new System.Drawing.Size(75, 23);
+            this.containsButton.TabIndex = 5;
+            this.containsButton.Text = "Проверить";
+            this.containsButton.UseVisualStyleBackColor = true;
+            this.containsButton.Click += new System.EventHandler(this.containsButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(12, 102);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(62, 13);
+            this.resultLabel.TabIndex = 6;
+            this.resultLabel.Text = "Результат:";
+            // 
+            // multiplierTextBox
+            // 
+            this.multiplierTextBox.Location = new System.Drawing.Point(19, 160);
+            this.multiplierTextBox.Name = "multiplierTextBox";
+            this.multiplierTextBox.Size = new System.Drawing.Size(100, 20);
+            this.multiplierTextBox.TabIndex = 7;
+            // 
+            // characterTextBox
+            // 
+            this.characterTextBox.Location = new System.Drawing.Point(19, 186);
+            this.characterTextBox.Name = "characterTextBox";
+            this.characterTextBox.Size = new System.Drawing.Size(100, 20);
+            this.characterTextBox.TabIndex = 8;
+            // 
+            // removeNegativeButton
+            // 
+            this.removeNegativeButton.Location = new System.Drawing.Point(301, 233);
+            this.removeNegativeButton.Name = "removeNegativeButton";
+            this.removeNegativeButton.Size = new System.Drawing.Size(123, 23);
+            this.removeNegativeButton.TabIndex = 9;
+            this.removeNegativeButton.Text = "Удалить отриц";
+            this.removeNegativeButton.UseVisualStyleBackColor = true;
+            this.removeNegativeButton.Click += new System.EventHandler(this.removeNegativeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 265);
+            this.Controls.Add(this.removeNegativeButton);
+            this.Controls.Add(this.characterTextBox);
+            this.Controls.Add(this.multiplierTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.containsButton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.elementTextBox);
+            this.Controls.Add(this.arrayContentsLabel);
+            this.Controls.Add(this.arraySizeLabel);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label arraySizeLabel;
+        private System.Windows.Forms.Label arrayContentsLabel;
+        private System.Windows.Forms.TextBox elementTextBox;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button containsButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox multiplierTextBox;
+        private System.Windows.Forms.TextBox characterTextBox;
+        private System.Windows.Forms.Button removeNegativeButton;
+    }
+}
+
---
+++ день 16/App/App/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        private OneDimensionalArray array;
+        public Form1()
+        {
+            InitializeComponent();
+            array = new OneDimensionalArray();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int value;
+            if (int.TryParse(elementTextBox.Text, out value))
+            {
+                array.Add(value);
+                elementTextBox.Clear();
+                UpdateArrayInfo();
+            }
+            else
+            {
+                MessageBox.Show("Invalid element value. Please enter an integer.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void UpdateArrayInfo()
+        {
+            arraySizeLabel.Text = $"Size: {array.Size}";
+            arrayContentsLabel.Text = $"Contents: {array}";
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            int value;
+            if (int.TryParse(multiplierTextBox.Text, out value))
+            {
+                OneDimensionalArray multipliedArray = array * value;
+                resultLabel.Text = multipliedArray.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid multiplier value. Please enter an integer.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void containsButton_Click(object sender, EventArgs e)
+        {
+            char character = characterTextBox.Text.FirstOrDefault();
+            bool contains = array.ContainsCharacter(character);
+            resultLabel.Text = contains ? "Array contains the specified character." : "Array does not contain the specified character.";
+        }
+
+        private void removeNegativeButton_Click(object sender, EventArgs e)
+        {
+            array.RemoveNegativeElements();
+            UpdateArrayInfo();
+        }
+    }
+    public class OneDimensionalArray
+    {
+        private int[] array;
+
+        public int Size => array.Length;
+
+        public int this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+
+        public OneDimensionalArray()
+        {
+            array = new int[0];
+        }
+
+        public void Add(int element)
+        {
+            Array.Resize(ref array, Size + 1);
+            array[Size - 1] = element;
+        }
+
+        public static OneDimensionalArray operator *(OneDimensionalArray array, int multiplier)
+        {
+            OneDimensionalArray result = new OneDimensionalArray();
+            for (int i = 0; i < array.Size; i++)
+            {
+                result.Add(array[i] * multiplier);
+            }
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is OneDimensionalArray otherArray)
+            {
+                if (Size != otherArray.Size)
+                    return false;
+
+                for (int i = 0; i < Size; i++)
+                {
+                    if (array[i] != otherArray[i])
+                        return false;
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return array.GetHashCode();
+        }
+
+        public bool ContainsCharacter(char character)
+        {
+            foreach (int element in array)
+            {
+                if (element.ToString().Contains(character.ToString()))
+                    return true;
+            }
+            return false;
+        }
+
+        public void RemoveNegativeElements()
+        {
+            array = array.Where(x => x >= 0).ToArray();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", array);
+        }
+    }
+    public static class StringExtensions
+    {
+        public static bool ContainsCharacter(this string str, char character)
+        {
+            return str.Contains(character);
+        }
+    }
+
+}
