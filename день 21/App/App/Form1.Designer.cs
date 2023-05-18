299cb955d6a67736fa947bb2198363352052119e
PaXan <116594902+Paxanchik@users.noreply.github.com>
2023-05-18 13:23:40 +0300

    Add files via upload

---
+++ день 21/App/App/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numAge = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbSpecialty = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numCourse = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtGroup = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numGPA = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radMale = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.radFemale = new System.Windows.Forms.RadioButton();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.txtHouseNumber = new System.Windows.Forms.TextBox();
+            this.txtApartmentNumber = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnSaveToFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCourse)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numGPA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(121, 23);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ФИО";
+            // 
+            // numAge
+            // 
+            this.numAge.Location = new System.Drawing.Point(121, 49);
+            this.numAge.Name = "numAge";
+            this.numAge.Size = new System.Drawing.Size(120, 20);
+            this.numAge.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Возраст";
+            // 
+            // cmbSpecialty
+            // 
+            this.cmbSpecialty.FormattingEnabled = true;
+            this.cmbSpecialty.Location = new System.Drawing.Point(121, 75);
+            this.cmbSpecialty.Name = "cmbSpecialty";
+            this.cmbSpecialty.Size = new System.Drawing.Size(121, 21);
+            this.cmbSpecialty.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Специальность";
+            // 
+            // dateBirthDate
+            // 
+            this.dateBirthDate.Location = new System.Drawing.Point(121, 102);
+            this.dateBirthDate.Name = "dateBirthDate";
+            this.dateBirthDate.Size = new System.Drawing.Size(200, 20);
+            this.dateBirthDate.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Дата рождения";
+            // 
+            // numCourse
+            // 
+            this.numCourse.Location = new System.Drawing.Point(121, 128);
+            this.numCourse.Name = "numCourse";
+            this.numCourse.Size = new System.Drawing.Size(120, 20);
+            this.numCourse.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Курс";
+            // 
+            // txtGroup
+            // 
+            this.txtGroup.Location = new System.Drawing.Point(121, 154);
+            this.txtGroup.Name = "txtGroup";
+            this.txtGroup.Size = new System.Drawing.Size(100, 20);
+            this.txtGroup.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Группа";
+            // 
+            // numGPA
+            // 
+            this.numGPA.Location = new System.Drawing.Point(121, 180);
+            this.numGPA.Name = "numGPA";
+            this.numGPA.Size = new System.Drawing.Size(120, 20);
+            this.numGPA.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Ср. балл";
+            // 
+            // radMale
+            // 
+            this.radMale.AutoSize = true;
+            this.radMale.Location = new System.Drawing.Point(121, 215);
+            this.radMale.Name = "radMale";
+            this.radMale.Size = new System.Drawing.Size(34, 17);
+            this.radMale.TabIndex = 14;
+            this.radMale.TabStop = true;
+            this.radMale.Text = "М";
+            this.radMale.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 215);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Пол";
+            // 
+            // radFemale
+            // 
+            this.radFemale.AutoSize = true;
+            this.radFemale.Location = new System.Drawing.Point(185, 215);
+            this.radFemale.Name = "radFemale";
+            this.radFemale.Size = new System.Drawing.Size(36, 17);
+            this.radFemale.TabIndex = 16;
+            this.radFemale.TabStop = true;
+            this.radFemale.Text = "Ж";
+            this.radFemale.UseVisualStyleBackColor = true;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(121, 238);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 241);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Город";
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(121, 264);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtPostalCode.TabIndex = 19;
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(121, 290);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(100, 20);
+            this.txtStreet.TabIndex = 20;
+            // 
+            // txtHouseNumber
+            // 
+            this.txtHouseNumber.Location = new System.Drawing.Point(121, 316);
+            this.txtHouseNumber.Name = "txtHouseNumber";
+            this.txtHouseNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtHouseNumber.TabIndex = 21;
+            // 
+            // txtApartmentNumber
+            // 
+            this.txtApartmentNumber.Location = new System.Drawing.Point(121, 342);
+            this.txtApartmentNumber.Name = "txtApartmentNumber";
+            this.txtApartmentNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtApartmentNumber.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 267);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(45, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Индекс";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 293);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Улица";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 319);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(70, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "Номер дома";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 345);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(55, 13);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Квартира";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(35, 383);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(77, 23);
+            this.btnSave.TabIndex = 27;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(144, 383);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(77, 23);
+            this.btnShow.TabIndex = 28;
+            this.btnShow.Text = "Показать";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnSaveToFile
+            // 
+            this.btnSaveToFile.Location = new System.Drawing.Point(244, 383);
+            this.btnSaveToFile.Name = "btnSaveToFile";
+            this.btnSaveToFile.Size = new System.Drawing.Size(136, 23);
+            this.btnSaveToFile.TabIndex = 29;
+            this.btnSaveToFile.Text = "Сохранить в файлы";
+            this.btnSaveToFile.UseVisualStyleBackColor = true;
+            this.btnSaveToFile.Click += new System.EventHandler(this.btnSaveToFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 433);
+            this.Controls.Add(this.btnSaveToFile);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtApartmentNumber);
+            this.Controls.Add(this.txtHouseNumber);
+            this.Controls.Add(this.txtStreet);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.radFemale);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.radMale);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numGPA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtGroup);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numCourse);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateBirthDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbSpecialty);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numAge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCourse)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numGPA)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numAge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbSpecialty;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateBirthDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numCourse;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtGroup;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numGPA;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radMale;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton radFemale;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.TextBox txtHouseNumber;
+        private System.Windows.Forms.TextBox txtApartmentNumber;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnSaveToFile;
+    }
+}
+
---
+++ день 21/App/App/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> students = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbSpecialty.Items.AddRange(new string[] { "Математика", "Физика", "Химия", "Информатика" });
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Валидация вводимых данных
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Введите ФИО студента");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(cmbSpecialty.Text))
+            {
+                MessageBox.Show("Выберите специальность");
+                return;
+            }
+
+            // Создание и добавление студента
+            var student = new Student()
+            {
+                Name = txtName.Text,
+                Age = (int)numAge.Value,
+                Specialty = cmbSpecialty.Text,
+                BirthDate = dateBirthDate.Value,
+                Course = (int)numCourse.Value,
+                Group = txtGroup.Text,
+                GPA = (double)numGPA.Value,
+                Gender = radMale.Checked ? "Мужской" : "Женский",
+                Address = new Address()
+                {
+                    City = txtCity.Text,
+                    PostalCode = txtPostalCode.Text,
+                    Street = txtStreet.Text,
+                    HouseNumber = txtHouseNumber.Text,
+                    ApartmentNumber = txtApartmentNumber.Text
+                }
+            };
+
+            students.Add(student);
+            ClearForm();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            // Отображение сохраненных данных
+            var output = "";
+
+            foreach (var student in students)
+            {
+                output += "ФИО: " + student.Name + Environment.NewLine;
+                output += "Возраст: " + student.Age + Environment.NewLine;
+                output += "Специальность: " + student.Specialty + Environment.NewLine;
+                output += "Дата рождения: " + student.BirthDate.ToShortDateString() + Environment.NewLine;
+                output += "Курс: " + student.Course + Environment.NewLine;
+                output += "Группа: " + student.Group + Environment.NewLine;
+                output += "Средний балл: " + student.GPA + Environment.NewLine;
+                output += "Пол: " + student.Gender + Environment.NewLine;
+                output += "Адрес: " + Environment.NewLine;
+                output += "Город: " + student.Address.City + Environment.NewLine;
+                output += "Индекс: " + student.Address.PostalCode + Environment.NewLine;
+                output += "Улица: " + student.Address.Street + Environment.NewLine;
+                output += "Дом: " + student.Address.HouseNumber + Environment.NewLine;
+                output += "Квартира: " + student.Address.ApartmentNumber + Environment.NewLine;
+                output += Environment.NewLine;
+            }
+
+            MessageBox.Show(output);
+        }
+        private void ClearForm()
+        {
+            // Очистка полей формы
+            txtName.Text = "";
+            numAge.Value = 0;
+            cmbSpecialty.SelectedIndex = -1;
+            dateBirthDate.Value = DateTime.Now;
+            numCourse.Value = 1;
+            txtGroup.Text = "";
+            numGPA.Value = 0;
+            radMale.Checked = true;
+            txtCity.Text = "";
+            txtPostalCode.Text = "";
+            txtStreet.Text = "";
+            txtHouseNumber.Text = "";
+            txtApartmentNumber.Text = "";
+        }
+
+        private void btnSaveToFile_Click(object sender, EventArgs e)
+        {
+            // Сохранение данных в XML и JSON файлы
+            using (var xmlWriter = XmlWriter.Create("students.xml"))
+            {
+                var serializer = new XmlSerializer(typeof(List<Student>));
+                serializer.Serialize(xmlWriter, students);
+            }
+
+            var json = JsonConvert.SerializeObject(students);
+            System.IO.File.WriteAllText("students.json", json);
+
+            MessageBox.Show("Данные сохранены в файлы XML и JSON");
+        }
+    }
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Specialty { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Course { get; set; }
+        public string Group { get; set; }
+        public double GPA { get; set; }
+        public string Gender { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+    }
+}
