d2b5178fff90585bf2b1df27fe08e523b91a8848
PaXan <116594902+Paxanchik@users.noreply.github.com>
2023-05-30 13:39:22 +0300

    Add files via upload

---
+++ день 22/App/App/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        private SmartphoneFactory smartphoneFactory;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIPhone_Click(object sender, EventArgs e)
+        {
+            smartphoneFactory = new IPhoneFactory();
+            smartphoneFactory.DisplaySmartphoneInfo();
+        }
+
+        private void btnSamsungGalaxy_Click(object sender, EventArgs e)
+        {
+            smartphoneFactory = new SamsungGalaxyFactory();
+            smartphoneFactory.DisplaySmartphoneInfo();
+        }
+    }
+    public interface ISmartphone
+    {
+        string GetModel();
+        string GetProcessor();
+        int GetRAM();
+    }
+    // Конкретный продукт
+    public class IPhone : ISmartphone
+    {
+        public string GetModel()
+        {
+            return "iPhone X";
+        }
+
+        public string GetProcessor()
+        {
+            return "Apple A11 Bionic";
+        }
+
+        public int GetRAM()
+        {
+            return 3;
+        }
+    }
+    // Конкретный продукт
+    public class SamsungGalaxy : ISmartphone
+    {
+        public string GetModel()
+        {
+            return "Samsung Galaxy S20";
+        }
+
+        public string GetProcessor()
+        {
+            return "Qualcomm Snapdragon 865";
+        }
+
+        public int GetRAM()
+        {
+            return 8;
+        }
+    }
+    // Создатель
+    public abstract class SmartphoneFactory
+    {
+        public abstract ISmartphone CreateSmartphone();
+
+        public void DisplaySmartphoneInfo()
+        {
+            ISmartphone smartphone = CreateSmartphone();
+            MessageBox.Show($"Model: {smartphone.GetModel()}\nProcessor: {smartphone.GetProcessor()}\nRAM: {smartphone.GetRAM()} GB");
+        }
+    }
+
+    // Конкретный создатель
+    public class IPhoneFactory : SmartphoneFactory
+    {
+        public override ISmartphone CreateSmartphone()
+        {
+            return new IPhone();
+        }
+    }
+    // Конкретный создатель
+    public class SamsungGalaxyFactory : SmartphoneFactory
+    {
+        public override ISmartphone CreateSmartphone()
+        {
+            return new SamsungGalaxy();
+        }
+    }
+}
---
+++ день 23/App/App/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        private Hospital hospital;
+        public Form1()
+        {
+            InitializeComponent();
+            hospital = new Hospital();
+
+            // Добавляем пациентов в больницу
+            hospital.AddPatient(new Patient { Id = 1, Name = "Иванов Иван Иванович", DateOfBirth = new DateTime(1990, 5, 1), Diagnosis = "Грипп", IsAdmitted = true });
+            hospital.AddPatient(new Patient { Id = 2, Name = "Петров Петр Петрович", DateOfBirth = new DateTime(1980, 12, 15), Diagnosis = "Ангина", IsAdmitted = true });
+            hospital.AddPatient(new Patient { Id = 3, Name = "Сидоров Сидор Сидорович", DateOfBirth = new DateTime(1975, 8, 23), Diagnosis = "Простуда", IsAdmitted = false });
+            hospital.AddPatient(new Patient { Id = 4, Name = "Кузнецова Екатерина Владимировна", DateOfBirth = new DateTime(1995, 3, 10), Diagnosis = "Перелом", IsAdmitted = false });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var patients = hospital.GetPatients().Where(p => p.IsAdmitted);
+            listBox1.Items.Clear();
+
+            foreach (var patient in patients)
+            {
+                listBox1.Items.Add($"{patient.Name} ({patient.DateOfBirth.ToShortDateString()})");
+            }
+        }
+    }
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Diagnosis { get; set; }
+        public bool IsAdmitted { get; set; }
+    }
+    public class Hospital
+    {
+        private List<Patient> patients;
+
+        public Hospital()
+        {
+            patients = new List<Patient>();
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            patients.Add(patient);
+        }
+
+        public void RemovePatient(Patient patient)
+        {
+            patients.Remove(patient);
+        }
+
+        public List<Patient> GetPatients()
+        {
+            return patients;
+        }
+    }
+}
